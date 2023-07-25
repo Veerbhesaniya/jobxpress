d378b328041e6bf2e74831cae271d88057f6e93d
DELL <bhesaniyq@gmail.com>
2023-07-26 05:09:55 +0530

    last commit

--- src/Components/Emp_Registration.jsx
+++ src/Components/Emp_Registration.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Navbar from './Navbar';
+// import Navbar from './Navbar';
 import "../StyleSheets/Reg.css";
 // import { json } from 'express';
 function Empregistration({ screen, setScreen }) {
